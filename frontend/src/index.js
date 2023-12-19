c7d170b1383bdd1f4cdbe96ead6079d80c57d745
thangbuiq <buiquangthangtv@gmail.com>
2023-12-20 06:59:36 +0700

    hotfix(react): remove strict mode

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,9 +5,9 @@ import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>
+  </BrowserRouter>
 );
 
 
