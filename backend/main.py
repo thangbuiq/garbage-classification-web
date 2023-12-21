2137dec93ff4b60fce7e13270fb8b5d424378124
Thang Bui Quang <89576896+thangbuiq@users.noreply.github.com>
2023-12-21 07:22:22 +0000

    hotfix: docker

--- backend/main.py
+++ backend/main.py
@@ -46,7 +46,7 @@ async def upload_image(file: UploadFile = File(...)):
 if __name__ == "__main__":
     uvicorn.run(
         "main:app",
-        host="0.0.0.0",
+        host="172.31.37.25",
         port=8000,
         reload=True
     )
