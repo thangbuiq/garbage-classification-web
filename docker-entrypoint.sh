898b44ce658b2a07d6656cd96428e737fb99b20c
thangbuiq <buiquangthangtv@gmail.com>
2023-12-22 16:22:54 +0700

    hotfix

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,6 +4,9 @@ echo "$0: Starting the application"
 
 export PUBLIC_IP_ADDRESS=$(curl http://checkip.amazonaws.com)
 
+# Replace the javascript placeholder with the actual IP address
+sed -i "s|PUBLIC_IP_ADDRESS|$PUBLIC_IP_ADDRESS:8000|g" /usr/share/nginx/html/static/js/*.js
+
 # Start Nginx in the foreground
 nginx -g "daemon off;" &
 
