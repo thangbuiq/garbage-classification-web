ed015a1280c844a8fe842e884a928225406e1b18
lvbaotram <levobaotram@gmail.com>
2023-12-24 20:24:27 +0700

    update file give a advice

--- backend/main.py
+++ backend/main.py
@@ -7,6 +7,10 @@ from utils import model, predict
 import uvicorn
 import shutil
 import os
+import openai
+from dotenv import load_dotenv
+load_dotenv()
+openai.api_key = os.getenv("API_KEY")
 
 PUBLIC_IP_ADDRESS = os.environ.get("PUBLIC_IP_ADDRESS")
 PUBLIC_DNS_ADDRESS = os.environ.get("PUBLIC_DNS_ADDRESS")
@@ -19,6 +23,22 @@ origins = [
     "http://localhost:8000", # For debugging
     "http://localhost:3000", # For debugging
 ]
+
+class trash(BaseModel):
+    type_trash:  str
+
+def input_trash(input):
+    messages = [
+    {"role": "system", "content":"Act as an environmental advocate, your task offers a brief simple and friendly tip on handling garbage."}
+    ]
+    messages.append(
+        {"role": "user", "content": f"{input}"},
+)
+    chat = openai.ChatCompletion.create(
+        model = 'gpt-3.5-turbo', messages=messages
+    )
+    reply = chat.choices[0].message.content
+    return reply
 
 app = FastAPI(
     title="🗑️ Garbage Classification",
@@ -78,6 +98,10 @@ async def predict_endpoint(file: UploadFile = File(...)):
         "predicted_value": predicted_value,
         "predicted_accuracy": predicted_accuracy
     }
+
+async def give_advice(Trash: trash ):
+    advice = input_trash(f"Garbage name: {Trash.type_trash}")
+    return {"Advice": advice}
 
 if __name__ == "__main__":
     uvicorn.run(
