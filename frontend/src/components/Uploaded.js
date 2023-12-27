b75392ace09db1a78ff370bf41b443abb2468b17
thangbuiq <buiquangthangtv@gmail.com>
2023-12-27 21:49:29 +0700

    feat(ui): api integrate with frontend

--- frontend/src/components/Uploaded.js
+++ frontend/src/components/Uploaded.js
@@ -1,16 +1,39 @@
 //import CheckIcon from '@mui/icons-material/Check';
-const Uploaded = ({ image, predict }) => {
+
+const Uploaded = ({ image, predict, advice, color, TrashBinImage }) => {
+  const handleRetry = () => {
+    window.location.reload();
+  };
   return (
-    <div className="flex flex-col sm:drop-shadow-lg gap-6 py-16 items-center justify-center w-full mx-4 bg-white md:w-3/5 rounded-3xl">
-      <p className="text-center font-semibold text-2xl uppercase text-[#8BC541]">garbage classification</p>
-      <div>
-        <img src={image} className="max-w-full mx-auto h-[18vh] sm:h-[28vh] rounded-3xl" />
+    <>
+      <div className="fixed left-0 top-1/2 -translate-y-1/2 sm:drop-shadow-lg gap-6 py-16 items-center justify-center w-2/5 mx-4 bg-white md:w-3/5 rounded-3xl transition-all duration-500 transform translate-x- sm:translate-x-40 hidden sm:block">
+        <div className="flex items-center">
+          <img src={TrashBinImage} alt="Trash Bin" className="h-16" />
+          <p className={`ml-0 uppercase text font-bold ${color}`}>{predict}</p>
+        </div>
       </div>
-      <div className="flex justify-center">
-        <p className="text-center font-thin text-xs text-slate-600 mb-2">Detected</p>
-        <div className="bg-lime-400/40 text-slate-600 text-md rounded-xl px-8 py-3 mx-12 capitalize">{predict}</div>
+      <div className="flex flex-col sm:drop-shadow-lg gap-6 py-16 items-center justify-center w-full mx-4 bg-white md:w-1/2 rounded-3xl">
+        <p className="text-center font-semibold text-2xl uppercase text-[#8BC541]">garbage classification</p>
+        <div>
+          <img src={image} className="max-w-full mx-auto h-[28vh] sm:h-[28vh] rounded-3xl" />
+        </div>
+        <div className="flex justify-center">
+          <div className="bg-lime-200/40 text-slate-600 text-md rounded-xl px-8 py-3 mx-12">
+            {advice.split('\n').map((item, i) => (
+              <p key={i} className="mt-2">
+                {item}
+              </p>
+            ))}
+          </div>
+        </div>
+        <button
+          onClick={handleRetry}
+          className="bg-lime-400/40 text-slate-600 font-medium rounded-xl w-auto mx-auto px-6 py-3 text-md hover:bg-lime-600/75 hover:text-white transition-all duration-300"
+        >
+          Retry
+        </button>
       </div>
-    </div>
+    </>
   );
 };
 
